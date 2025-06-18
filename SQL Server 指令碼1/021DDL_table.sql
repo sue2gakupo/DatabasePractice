43c68cd5ef55436ba85520f5b494616e1f1479e6
SuuYii <sue2gakupo@gmail.com>
2025-06-18 16:27:51 +0800

    feat(files):create new tables

--- SQL Server 指令碼1/021DDL_table.sql
+++ SQL Server 指令碼1/021DDL_table.sql
@@ -1,5 +1,6 @@
 --建立資料夾
 
+--訂單資料表
 create table[Product](
 ProductID nchar(5) not null primary key,
 ProductName nvarchar(40) not null,
@@ -8,3 +9,27 @@ Price money not null check(Price>=0),
 Picture nvarchar(12) not null,
 CreateDateID datetime not null default getdate() 
 )
+
+--會員資料表
+create table[Member](
+MemberID nchar(6) not null primary key,
+[Name] nvarchar(30) not null,
+Gender bit not null,
+--位元資料是放1個位元的大小
+MemberPoint int not null,
+Account nvarchar(12) not null unique,
+--儲存為唯一值
+Password nvarchar(20) not null,
+CreatedDate datetime not null default getdate()
+)
+
+--訂單資料表
+create table[Order](
+OrderID nchar (12) not null primary key, --colume level
+OrderDate datetime not null default getdate(),
+MemberID nchar (6) not null, 
+ContactName nvarchar (30) not null, 
+ContactAddress nvarchar (100) not null, 
+foreign key(MemberID) references [Member](MemberID) --table level
+)
+
